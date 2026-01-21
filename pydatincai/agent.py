32acc5e72021effc6481e3b8a658f88f4aa3620b
wuwenrufeng <wuwenrufeng@163.com>
2026-01-21 23:03:45 +0800

    refactor(pydanticai): ck数据库查询优化

--- pydatincai/agent.py
+++ pydatincai/agent.py
@@ -23,33 +23,38 @@ class Column(BaseModel):
     comment: str
 
 
-agent = agent.Agent(
+db_agent = agent.Agent(
     "openai:kimi-k2-0711-preview",
     system_prompt="你是一个ClickHouse 表结构查询助手",
     deps_type=Config,
 )
 
 
-@agent.tool
+@db_agent.tool
 async def list_database(ctx: RunContext[Config]) -> List[str]:
     result = await ctx.deps.ck_client.query("SHOW DATABASES")
     databases = [row[0] for row in result.result_rows]
     return databases
 
 
-@agent.tool
+@db_agent.tool
 async def list_tables(ctx: RunContext[Config], database) -> List[str]:
     result = await ctx.deps.ck_client.query(f"SHOW TABLES IN {database}")
     tables = [row[0] for row in result.result_rows]
     return tables
 
 
-@agent.tool
+@db_agent.tool
 async def describe_table(ctx: RunContext[Config], database, table: str) -> List[Column]:
     result = await ctx.deps.ck_client.query(f'DESCRIBE TABLE "{database}"."{table}"')
     columns = []
-    for row in result.result_rows():
-        column = Column(name=row["name"], type=row["type"], comment=row["comment"])
+    for row in result.result_rows:
+        item = dict(zip(result.column_names, row))
+        column = Column(
+            name=item.get("name", ""),
+            type=item.get("type", ""),
+            comment=item.get("comment", ""),
+        )
         columns.append(column)
     return columns
 
@@ -64,21 +69,10 @@ async def main():
         user_input = input("User: ")
         if user_input == "q":
             break
-        result = await agent.run(user_input, deps=config, message_history=history)
+        result = await db_agent.run(user_input, deps=config, message_history=history)
         history.extend(result.new_messages())
         print("Agent: ", result.output)
 
 
-async def test_query():
-    client = await clickhouse_connect.get_async_client(dsn=os.getenv("CLICKHOUSE_DSN"))
-    result = await client.query("SHOW DATABASES")
-    print("类型:", type(result.result_rows()))
-    print("第一行:", result.result_rows()[0] if result.result_rows() else None)
-    print(
-        "第一行类型:", type(result.result_rows()[0]) if result.result_rows() else None
-    )
-
-
 if __name__ == "__main__":
-    # asyncio.run(main())
-    asyncio.run(test_query())
+    asyncio.run(main())
